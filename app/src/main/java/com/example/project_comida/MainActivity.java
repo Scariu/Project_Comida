694e7565f866c2b735680870d93af1c770f4a7cd
Flopy <f.hidalgoalarcon@gmail.com>
2023-06-28 22:04:52 -0400

    Se modifica binding.

--- app/src/main/java/com/example/project_comida/MainActivity.java
+++ app/src/main/java/com/example/project_comida/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.renderscript.ScriptGroup;
+import android.view.View;
 
 import com.example.project_comida.databinding.ActivityMainBinding;
 
@@ -12,8 +13,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+       /*
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        */
+
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
         initListeners();
     }
 
