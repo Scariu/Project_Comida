33025c8780caed68853084ca50341b5224af30ae
Flopy <f.hidalgoalarcon@gmail.com>
2023-06-28 22:32:04 -0400

    Se agrega función a ListView en MenuActivity.

--- app/src/main/java/com/example/project_comida/MenuActivity.java
+++ app/src/main/java/com/example/project_comida/MenuActivity.java
@@ -3,9 +3,13 @@ package com.example.project_comida;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import com.example.project_comida.databinding.ActivityMainBinding;
 import com.example.project_comida.databinding.ActivityMenuBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MenuActivity extends AppCompatActivity {
     private ActivityMenuBinding binding;
@@ -15,5 +19,21 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMenuBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        initList();
+    }
+
+    private void initList() {
+        List<String> listaMenu = new ArrayList<>();
+        listaMenu.add("Papas fritas");
+        listaMenu.add("Completo italiano");
+        listaMenu.add("Churrasco italiano");
+        listaMenu.add("Chacarero");
+        listaMenu.add("Porotos con riendas");
+        listaMenu.add("Lentejas");
+        listaMenu.add("Pan con queso");
+        listaMenu.add("Leche con chocolate");
+
+        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listaMenu);
+        binding.lista.setAdapter(adapter);
     }
 }
