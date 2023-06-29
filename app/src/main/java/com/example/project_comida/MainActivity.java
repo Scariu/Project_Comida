1ca1e0aba629a399d9ca7b3201999a378cf0fd5e
Flopy <f.hidalgoalarcon@gmail.com>
2023-06-28 21:52:10 -0400

    Se agrega binding a Main.

--- app/src/main/java/com/example/project_comida/MainActivity.java
+++ app/src/main/java/com/example/project_comida/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.project_comida;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+
+import com.example.project_comida.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initListeners();
+    }
+
+    private void initListeners() {
     }
 }
